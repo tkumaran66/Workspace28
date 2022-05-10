ee9dce2b294158bd61b107b6af47efbfb2fd2e53
Thiru <tkumaran66@gmail.com>
2022-05-10 18:21:58 +0530

    2nd commit

--- 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/BranchTest1.java
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/BranchTest1.java
@@ -3,5 +3,6 @@ package com.tkcomp.sampleGitPjt;
 public class BranchTest1 {
 	public static void main(String args[]) {
 		System.out.println("Baranch testing");
+		System.out.println("Baranch testing2");
 	}
 }
