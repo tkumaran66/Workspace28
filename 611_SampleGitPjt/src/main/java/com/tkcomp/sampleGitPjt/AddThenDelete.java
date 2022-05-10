486dc055889ac455f6b2c9acd63e33667c33cb5b
Thiru <tkumaran66@gmail.com>
2022-05-10 15:45:23 +0530

    delete and move commit

---
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/AddThenDelete.java
@@ -0,0 +1,7 @@
+package com.tkcomp.sampleGitPjt;
+
+public class AddThenDelete {
+	public static void main(String args[]) {
+		System.out.println("Added and then delete.");
+	}
+}
--- 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/MovedSample.java
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/MovedSample.java
@@ -3,10 +3,11 @@ package com.tkcomp.sampleGitPjt;
 public class Sample {
 	public static void main(String args[]) {
 		System.out.println("Test data");
-		System.out.println("modified data");
+		System.out.println("modified data1");
 		System.out.println("staging data");
-		System.out.println("new data");
-		System.out.println("name changed data");
+		System.out.println("name changed data2");
 		System.out.println("direct commit");
+		
+		System.out.println("see diff commit");
 	}
 }
