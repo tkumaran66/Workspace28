43b742ca4d7763f5a145cfd6fd8573600e147315
Thiru <tkumaran66@gmail.com>
2022-05-10 18:27:44 +0530

    1st master commit from branch

---
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/Masterbranchtest.java
@@ -0,0 +1,9 @@
+package com.tkcomp.sampleGitPjt;
+
+public class Masterbranchtest {
+	public static void main(String args[]) {
+		System.out.println("Avoid this file");
+		System.out.println("remove to ignore");
+		System.out.println("master staging");
+	}
+}
