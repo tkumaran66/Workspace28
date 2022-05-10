5a62c2be7671ffcd42fc025d95596d2212a7592b
tkumaran66 <tkumaran66@gmail.com>
2022-05-10 13:17:17 +0530

    5th commit after 2018

--- 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/Sample.java
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/Sample.java
@@ -4,5 +4,7 @@ public class Sample {
 	public static void main(String args[]) {
 		System.out.println("Test data");
 		System.out.println("modified data");
+		System.out.println("staging data");
+		System.out.println("new data");
 	}
 }
