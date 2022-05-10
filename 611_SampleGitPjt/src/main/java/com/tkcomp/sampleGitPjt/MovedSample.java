1f26d633207e7f3e47197c4ff45345f821b9210e
Thiru <tkumaran66@gmail.com>
2022-05-11 00:20:31 +0530

    new commit

--- 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/MovedSample.java
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/MovedSample.java
@@ -2,12 +2,10 @@ package com.tkcomp.sampleGitPjt;
 
 public class MovedSample {
 	public static void main(String args[]) {
-		System.out.println("Test data");
+		System.out.println("Test data1");
 		System.out.println("modified data1");
 		System.out.println("staging data");
 		System.out.println("name changed data2");
 		System.out.println("direct commit");
-		
-		System.out.println("New line from dev branch for merge check");
 	}
 }
