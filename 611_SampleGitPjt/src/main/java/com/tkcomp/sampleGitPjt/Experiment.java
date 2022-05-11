de81d49d2750e41a733ffb79d7d95295b5c52a8b
Thiru <tkumaran66@gmail.com>
2022-05-11 16:11:37 +0530

    2nd commit in exp

--- 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/Experiment.java
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/Experiment.java
@@ -3,5 +3,7 @@ package com.tkcomp.sampleGitPjt;
 public class Experiment {
 	public static void main(String args[]) {
 		System.out.println("THis is from dev branch");
+		
+		System.out.println("THis is from exp2 branch");
 	}
 }
