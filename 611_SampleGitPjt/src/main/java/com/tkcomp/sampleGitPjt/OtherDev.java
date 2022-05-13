3f7be79defa46d624d742de8f3cd16dbf41fa1ad
Thiru <tkumaran66@gmail.com>
2022-05-13 20:48:18 +0530

    just before stash commit

--- 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/OtherDev.java
+++ 611_SampleGitPjt/src/main/java/com/tkcomp/sampleGitPjt/OtherDev.java
@@ -3,7 +3,9 @@ package com.tkcomp.sampleGitPjt;
 class OtherDev {
  
   public static void main (String args[]) {
-    System.out.println("New file from remote repo"); 
+    System.out.println("New file from remote repo");
+    
+    System.out.println("Before the stash"); 
   }
   
 }
